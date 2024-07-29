9bb6ffaba29239bb5fe12616d349051a896e2846
dikiap <adiki436@gmail.com>
2024-07-29 21:19:04 +0700

    add log file

--- back-end/src/main/java/com/tujuhsembilan/example/controller/LogController.java
+++ back-end/src/main/java/com/tujuhsembilan/example/controller/LogController.java
@@ -1,6 +1,8 @@
 package com.tujuhsembilan.example.controller;
 
 import com.tujuhsembilan.example.dto.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class LogController {
-
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
     @PostMapping("/log")
     public String receiveLog(@RequestBody Log log) {
-        System.out.println("Received log: " + log);
-        // Simpan log ke database atau file jika diperlukan
+//        System.out.println("Received log: " + log);
+        logger.info("Received log: " + log);
         return "Log received";
     }
 }
